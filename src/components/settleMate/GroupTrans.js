3156b4eacca39f1010137a3e63e0e130f4b7c54e
Tej7278-chippada <tejachippada45@gmail.com>
2025-01-27 10:15:40 +0530

    unwanted code cleared on GroupTrans component. Ui refined.

--- src/components/settleMate/GroupTrans.js
+++ src/components/settleMate/GroupTrans.js
@@ -1,18 +1,14 @@
 // components/settleMate/GroupTrans.js
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Box, Typography, Card, Avatar, Grid, useMediaQuery, IconButton,  Snackbar, Alert, Dialog, DialogTitle, DialogContent, DialogActions, Toolbar, Button,  } from '@mui/material';
+import { Box, Typography, Card, Avatar, Grid, useMediaQuery, IconButton,  Snackbar, Alert, Dialog, DialogTitle, DialogContent, DialogActions, } from '@mui/material';
 import apiClient from '../../utils/axiosConfig';
 import Layout from '../Layout';
 import { useTheme } from '@emotion/react';
-// import { Delete, Refresh } from '@mui/icons-material';
-// import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
-// import MenuOpenRoundedIcon from '@mui/icons-material/MenuOpenRounded';
-// import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import WidgetsRoundedIcon from '@mui/icons-material/WidgetsRounded';
 import GroupDetails from './GroupDetails';
 import CloseIcon from '@mui/icons-material/Close';
-import { Close } from '@mui/icons-material';
+import PlaylistAddRoundedIcon from '@mui/icons-material/PlaylistAddRounded';
 
 const GroupTrans = ({ groupId: propGroupId }) => {
   const { groupId: paramGroupId } = useParams(); // Get groupId from URL if available
@@ -21,11 +17,8 @@ const GroupTrans = ({ groupId: propGroupId }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const [isMediaReady, setIsMediaReady] = useState(false); // Track media query readiness
-//   const [loadingJoinCode, setLoadingJoinCode] = useState(false); // Track loading state for join code
   const [snackbar, setSnackbar] = useState({ open: false, message: '', severity: '' }); // Snackbar state
-//   const [confirmationDialog, setConfirmationDialog] = useState({ open: false, action: null });
   const [groupError, setGroupError] = useState(false); // Track if the group doesn't exist
-//   const [confirmationDialog1, setConfirmationDialog1] = useState({ open: false, action: null, data: null });
   const navigate = useNavigate(); // Initialize navigation
   const [groupDetailsId, setGroupDetailsId] = useState(null); // Store the selected group ID
 
@@ -64,38 +57,6 @@ const GroupTrans = ({ groupId: propGroupId }) => {
     fetchGroupDetails();
   }, [groupId, navigate]);
 
-//   const handleGenerateJoinCode = async () => {
-//     setLoadingJoinCode(true);
-//     try {
-//       const response = await apiClient.post(
-//         `/api/groups/${groupId}/generate-code`,
-//         {},
-//         { headers: { Authorization: `Bearer ${localStorage.getItem('authToken')}` } }
-//       );
-//       setGroup((prevGroup) => ({
-//         ...prevGroup,
-//         joinCode: response.data.joinCode,
-//         joinCodeExpiry: new Date(Date.now() + 60 * 60 * 1000).toISOString(), // 1-hour expiry
-//       }));
-//       // Show success message
-//       setSnackbar({
-//         open: true,
-//         message: 'New Join code generated successfully, & valid till an hour.',
-//         severity: 'success',
-//       });
-//     } catch (error) {
-//       console.error('Error generating new join code:', error);
-//       // Show error message
-//       setSnackbar({
-//         open: true,
-//         message: 'Unable to generate new Join code, please try again later.',
-//         severity: 'error',
-//       });
-//     } finally {
-//       setLoadingJoinCode(false);
-//     }
-//   };
-
   const handleCloseSnackbar = () => setSnackbar({ ...snackbar, open: false });
 
   if (!isMediaReady) {
@@ -127,80 +88,6 @@ const GroupTrans = ({ groupId: propGroupId }) => {
     );
   }
 
-//   const isAdmin = group.members.some(
-//     (member) => member.user._id === localStorage.getItem('userId') && member.role === 'Admin'
-//   );
-
-//   const handleDeleteGroup = async () => {
-//     try {
-//       await apiClient.delete(`/api/groups/${groupId}`, {
-//         headers: { Authorization: `Bearer ${localStorage.getItem('authToken')}` },
-//       });
-//       setSnackbar({ open: true, message: `Group ${group.groupName} deleted successfully.`, severity: 'success' });
-//       setConfirmationDialog({ open: false, action: null });
-//       // Refresh the page
-//       setTimeout(() => {
-//         window.location.reload();
-//       }, 1000);
-//     } catch (error) {
-//       setSnackbar({ open: true, message: 'Failed to delete the group.', severity: 'error' });
-//     }
-//   };
-
-//   const handleRemoveMember = async () => {
-//     const { memberId, memberUsername } = confirmationDialog1.data;
-//     try {
-//       await apiClient.post(
-//         `/api/groups/${groupId}/remove-member`,
-//         { memberId },
-//         { headers: { Authorization: `Bearer ${localStorage.getItem('authToken')}` } }
-//       );
-//       setSnackbar({ open: true, message: `Member ${memberUsername} removed successfully.`, severity: 'success' });
-//       // Update the group state to reflect the removal
-//       setGroup((prevGroup) => ({
-//         ...prevGroup,
-//         members: prevGroup.members.filter((member) => member.user._id !== memberId),
-//       }));
-//     } catch (error) {
-//       setSnackbar({ open: true, message: 'Failed to remove member.', severity: 'error' });
-//     } finally {
-//       setConfirmationDialog1({ open: false, action: null, data: null });
-//     }
-//   };
-
-//   const handleOpenConfirmation1 = (action, data) => {
-//     setConfirmationDialog1({ open: true, action, data });
-//   };
-
-//   const handleCloseConfirmation1 = () => {
-//     setConfirmationDialog1({ open: false, action: null, data: null });
-//   };
-
-
-//   const handleExitGroup = async () => {
-//     try {
-//       await apiClient.post(`/api/groups/${groupId}/exit`, {}, {
-//         headers: { Authorization: `Bearer ${localStorage.getItem('authToken')}` },
-//       });
-//       setSnackbar({ open: true, message: `You have left the group ${group.groupName} .`, severity: 'success' });
-//       setConfirmationDialog({ open: false, action: null });
-//       // Refresh the page
-//       setTimeout(() => {
-//         window.location.reload();
-//       }, 1000);
-//     } catch (error) {
-//       setSnackbar({ open: true, message: 'Failed to leave the group.', severity: 'error' });
-//     }
-//   };
-
-//   const handleOpenConfirmation = (action) => {
-//     setConfirmationDialog({ open: true, action });
-//   };
-
-//   const handleCloseConfirmation = () => {
-//     setConfirmationDialog({ open: false, action: null });
-//   };
-
   const handleGroupClick = (group) => {
     setGroupDetailsId(group._id);
     if (isMobile) {
@@ -210,51 +97,6 @@ const GroupTrans = ({ groupId: propGroupId }) => {
 
   const content = (
     <Box p={isMobile ? '0px' : 0} position="relative" sx={{scrollbarWidth:'none'}}>
-      {/* <Card sx={{ p: (isMobile ? '6px' : 3), display: 'flex', justifyContent: 'space-between' }}>
-
-        <Box display="flex" alignItems="center">
-          <Avatar
-            alt={group.groupName[0]}
-            src={
-              group.groupPic
-                ? `data:image/jpeg;base64,${group.groupPic}`
-                : undefined
-            }
-            sx={{ width: 100, height: 100, mr: 2 }}
-          >{group.groupName[0]}</Avatar>
-          <div>
-            <Typography variant="h5">{group.groupName}
-              <IconButton
-                color="error"
-                onClick={() => handleOpenConfirmation(isAdmin ? 'delete' : 'exit')}
-                sx={{ ml: 1 }}
-              >
-                {(isAdmin ? <Delete /> : <LogoutRoundedIcon />)}
-              </IconButton>
-            </Typography>
-            <Typography variant='body2' sx={{  color: 'GrayText', display: 'inline-block', float: 'right' }}>
-              Group created on : <small>{new Date(group.createdAt).toLocaleString()}</small>
-            </Typography>
-          </div>
-        </Box>
-        <div>
-          <Box display="flex" alignItems="center">
-            <Typography variant="subtitle1">Join Code: {group.joinCode}</Typography>
-            {isAdmin && (
-              <IconButton
-                onClick={handleGenerateJoinCode}
-                disabled={loadingJoinCode}
-                sx={{ ml: 1 }}
-              >
-                {loadingJoinCode ? <CircularProgress size={20} /> : <Refresh />}
-              </IconButton>
-            )}
-          </Box>
-          <Typography variant='body2' sx={{  color: 'GrayText', display: 'inline-block', float: 'right' }}>
-            Valid till : <small>{new Date(group.joinCodeExpiry).toLocaleString()}</small>
-          </Typography>
-        </div>
-      </Card> */}
       <Box
         position="sticky" //fixed
         top={0}
@@ -269,7 +111,6 @@ const GroupTrans = ({ groupId: propGroupId }) => {
         }}
       >
         <Box display="flex" justifyContent="space-between" alignItems="center" sx={{scrollbarWidth:'none'}}>
-          {/* <Typography position="relative" variant="h5">Groups</Typography> */}
           <Box display="flex" alignItems="center">
             <Avatar
               alt={group.groupName[0]}
@@ -292,28 +133,12 @@ const GroupTrans = ({ groupId: propGroupId }) => {
             <Box display="flex" alignItems="center">
               <IconButton
                   onClick={() => handleGroupClick(group)}
-                //   disabled={loadingJoinCode}
                   sx={{ ml: 0 }}
                 >
                   <WidgetsRoundedIcon />
                 </IconButton>
             </Box>
             </Box>
-          {/* <Box>
-            <IconButton
-              color="default"
-              // onClick={() => setOpenCreateGroup(true)}
-              sx={{ mr: 1 }}
-            >
-              <CloseIcon />
-            </IconButton>
-            <IconButton
-              color="default"
-              // onClick={() => setOpenJoinGroup(true)}
-            >
-              <CloseIcon />
-            </IconButton>
-          </Box> */}
         </Box>
       </Box>
       <Box height={isMobile ? 'calc(80vh - 64px)' : 'calc(75vh - 64px)'}
@@ -323,46 +148,9 @@ const GroupTrans = ({ groupId: propGroupId }) => {
       }}>
       
 
-      <Box mt={0} height={ isMobile ? 'calc(80vh)' : 'calc(67vh)'} bgcolor="#f5f5f5" sx={{scrollbarWidth:'none'}}>
+      <Box mt={0} height={ isMobile ? 'calc(71vh)' : 'calc(66vh)'} bgcolor="#f5f5f5" sx={{scrollbarWidth:'none'}}>
         <Typography variant="h6">Members:</Typography>
         <Grid container spacing={1}>
-          {/* {group.members.map((member) => (
-            <Grid item key={member.user._id} xs={12} sm={12} md={6}>
-              <Card sx={{ display: 'flex', alignItems: 'center', p: (isMobile ? '6px' : 1), justifyContent: 'space-between' }}>
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                  <Avatar
-                    alt={member.user.username[0]}
-                    src={
-                      member.user.profilePic
-                        ? `data:image/jpeg;base64,${member.user.profilePic}`
-                        : undefined
-                    }
-                    sx={{ width: 56, height: 56, mr: 2 }}
-                  >{member.user.username[0]}</Avatar>
-                  <Box>
-                    <Typography>{member.user.username}</Typography>
-                    <Typography variant='body1' sx={{ color: member?.role === "Admin" ? "blue" : "grey" }}>{member.role}</Typography>
-                    <Typography variant='body2' sx={{ color: 'GrayText', display: 'inline-block', float: 'right' }}>
-                      Joined on : <small>{new Date(member.joined_at).toLocaleString()}</small>
-                    </Typography>
-                  </Box>
-                </Box>
-                {isAdmin && member.role === "Member" && (
-                  <IconButton
-                    color="error"
-                    onClick={() => handleOpenConfirmation1('removeMember', {
-                      memberId: member.user._id,
-                      memberUsername: member.user.username,
-                    })
-                    }
-                    aria-label="Delete Member"
-                  >
-                    <LogoutRoundedIcon />
-                  </IconButton>
-                )}
-              </Card>
-            </Grid>
-          ))} */}
         </Grid>
         <IconButton
           // onClick={(event) => {
@@ -399,7 +187,7 @@ const GroupTrans = ({ groupId: propGroupId }) => {
               Remove from Wishlist
             </span>
           )} */}
-          <Close />
+          <PlaylistAddRoundedIcon />
         </IconButton>
       </Box>
       {/* <Toolbar sx={{
@@ -472,46 +260,6 @@ const GroupTrans = ({ groupId: propGroupId }) => {
           {snackbar.message}
         </Alert>
       </Snackbar>
-      {/* <Dialog open={confirmationDialog.open} onClose={handleCloseConfirmation}>
-        <DialogTitle>
-          {confirmationDialog.action === 'delete' ? 'Delete Group' : 'Leave Group'}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Are you sure you want to {confirmationDialog.action === 'delete' ? 'delete this group permanently' : 'leave this group'}?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseConfirmation}>Cancel</Button>
-          <Button
-            onClick={
-              confirmationDialog.action === 'delete' ? handleDeleteGroup : handleExitGroup
-            }
-            color="error"
-          >
-            Confirm
-          </Button>
-        </DialogActions>
-      </Dialog> */}
-      {/* <Dialog open={confirmationDialog1.open} onClose={handleCloseConfirmation1}>
-        <DialogTitle>Confirm Action</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            {confirmationDialog1.action === 'removeMember'
-              ? `Are you sure you want to remove ${confirmationDialog1.data?.memberUsername} from the group?`
-              : ''}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseConfirmation1}>Cancel</Button>
-          <Button
-            onClick={handleRemoveMember}
-            color="error"
-          >
-            Confirm
-          </Button>
-        </DialogActions>
-      </Dialog> */}
       <Dialog open={!!groupDetailsId} onClose={() => setGroupDetailsId(null)} /* onClose={handleCloseConfirmation1} */ maxWidth="md" fullWidth>
         <DialogTitle sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} >
           Group details
@@ -526,7 +274,6 @@ const GroupTrans = ({ groupId: propGroupId }) => {
           </IconButton>
         </DialogTitle>
         <DialogContent>
-          {/* <DialogContentText> */}
           {!isMobile && (<Card sx={{
             flex: 3, marginInline: '-1rem',
             height: '80vh', // Fixed height relative to viewport
@@ -536,27 +283,12 @@ const GroupTrans = ({ groupId: propGroupId }) => {
             // boxShadow: 3, // Shadow for a modern look
             scrollbarWidth: 'thin'
           }}>
-            {/* {groupDetailsId ? ( */}
               <Box sx={{ margin: '-1rem' }}>
                 <GroupDetails groupId={groupDetailsId} /> {/* // Use GroupDetails component */}
               </Box>
-              {/* 
-            ) : (
-              <Box sx={{ margin: '0rem', textAlign: 'center', marginTop: '1rem' }}>
-                <Typography variant="h6" color="grey">Select a group to see details</Typography>
-              </Box>
-            )} */}
           </Card>)}
-          {/* </DialogContentText> */}
         </DialogContent>
         <DialogActions>
-          {/* <Button onClick={handleCloseConfirmation1}>Cancel</Button> */}
-          {/* <Button
-            onClick={handleRemoveMember}
-            color="error"
-          >
-            Confirm
-          </Button> */}
         </DialogActions>
       </Dialog>
     </>
