388c7ce6e974d7db75e74d2ecbc465ba2ef7f567
Tej7278-chippada <tejachippada45@gmail.com>
2025-01-30 18:43:02 +0530

    PriviteRoute.js component updated for having the condition of userId on localstorage.

--- src/components/PriviteRoute.jsx
+++ src/components/PriviteRoute.jsx
@@ -4,8 +4,9 @@ import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
   const authToken = localStorage.getItem('authToken'); // Retrieve token from localStorage
+  const userId = localStorage.getItem('userId'); // Retrieve token from localStorage
 
-  if (!authToken) {
+  if (!authToken || !userId) {
     // If no token, redirect to root with a state message
     return <Navigate to="/" state={{ message: 'Please login first.' }} />;
   }
